46bd85d44792b021ebf13c114163655a3fae0d1d
Nicolas Temciuc <nicotemciuc@gmail.com>
2025-09-02 18:08:36 -0300

    refactor: SVMWithSGDExample

--- sgd/src/main/scala/SVMWithSGDExample.scala
+++ sgd/src/main/scala/SVMWithSGDExample.scala
@@ -1,41 +1,64 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 // scalastyle:off println
 package org.apache.spark.examples.mllib
+
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
-import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
 
 object SVMWithSGDExample {
 
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("SVMWithSGDExample")
-    val sc = new SparkContext(conf)
+    val sc   = new SparkContext(conf)
 
-    val data = MLUtils.loadLibSVMFile(sc, "s3://morteza-files-ca/criteo.kaggle2014.svm/test.txt.svm")
+    // === Adjust these if needed ===
+    val csvPath    = sys.env.getOrElse("CSV_PATH", "file:///mnt/datasets/random_submission/random_submission.csv")
+    val hasHeader  = true
+    val delimiter  = ","        // String, not Char, so split(delimiter, -1) compiles on 2.12
+    val labelIndex = 0          // change if label is in another column
+    // ==============================
 
-    val splits = data.randomSplit(Array(0.8, 0.2), seed = 11L)
+    val lines = sc.textFile(csvPath)
+
+    val dataNoHeader =
+      if (hasHeader) {
+        val header = lines.first()
+        lines.filter(_ != header)
+      } else lines
+
+    def toDoubleSafe(s: String): Double =
+      try s.toDouble catch { case _: Throwable => 0.0 }
+
+    val labeled = dataNoHeader.flatMap { line =>
+      val parts = line.split(delimiter, -1).map(_.trim)
+      if (parts.length <= labelIndex) None
+      else {
+        val labelStr = parts(labelIndex)
+        val labelOpt =
+          try Some(labelStr.toDouble)
+          catch { case _: Throwable => None }
+
+        labelOpt.map { label =>
+          val feats = parts.indices.collect {
+            case i if i != labelIndex => toDoubleSafe(parts(i))
+          }.toArray
+          LabeledPoint(label, Vectors.dense(feats))
+        }
+      }
+    }.cache()
+
+    // Train/test split (avoid pattern binding to keep type inference simple)
+    val splits   = labeled.randomSplit(Array(0.8, 0.2), seed = 11L)
     val training = splits(0).cache()
-    val test = splits(1)
+    val test     = splits(1)
 
     val numIterations = 270000
-    val model = MySVMWithSGD.train(training, numIterations, 10, 0.01)
+    val stepSize      = 10.0
+    val regParam      = 0.01
+    val model         = MySVMWithSGD.train(training, numIterations, stepSize, regParam)
 
+    // Use raw margins for ROC
     model.clearThreshold()
 
     val scoreAndLabels = test.map { point =>
@@ -44,8 +67,7 @@ object SVMWithSGDExample {
     }
 
     val metrics = new BinaryClassificationMetrics(scoreAndLabels)
-    val auROC = metrics.areaUnderROC()
-
+    val auROC   = metrics.areaUnderROC()
     println(s"Area under ROC = $auROC")
 
     sc.stop()
