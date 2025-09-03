a7aab6368ad2976c6794b96976c9a082c5bc79a6
Nicolas Temciuc <nicotemciuc@gmail.com>
2025-09-03 18:40:28 -0300

    try 1

--- kmeans/src/main/scala/KMeans.scala
+++ kmeans/src/main/scala/KMeans.scala
@@ -5,57 +5,62 @@ import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.mllib.clustering.KMeans
 import org.apache.spark.mllib.linalg.{Vector, Vectors}
 
-import java.io.DataInputStream
+import java.io.{ByteArrayInputStream, DataInputStream}
 
 object KMeansExample {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("KMeansExample")
     val sc   = new SparkContext(conf)
 
-    // Pass path as arg(0), or default to a common location
-    val path = if (args.nonEmpty) args(0) else "/mnt/datasets/kmeans/t10k-images.idx3-ubyte"
+    // Pass the IDX images path as arg(0) if you like
+    val path = if (args.nonEmpty) args(0) else "/mnt/datasets/mnist/t10k-images.idx3-ubyte"
 
-    // --- Load MNIST IDX images (binary, big-endian) into an RDD[Vector] ---
-    // File structure:
-    // int magic(2051), int numImages, int numRows(28), int numCols(28), then images as unsigned bytes
+    // Read the single binary IDX file on executors.
+    // IMPORTANT: Read the entire file into a byte array FIRST so we can parse eagerly
+    // and avoid "Stream is closed!" issues from lazy iterators.
     val parsedData = sc.binaryFiles(path, minPartitions = 1).flatMap { case (_, pds) =>
-      val in = new DataInputStream(pds.open())
+      val bytes = pds.toArray() // read fully on the executor
+      val in = new DataInputStream(new ByteArrayInputStream(bytes))
       def readInt(): Int = in.readInt() // big-endian
-      try {
-        val magic     = readInt()
-        require(magic == 2051, s"Expected magic 2051 for images, got $magic")
-        val numImages = readInt()
-        val numRows   = readInt()
-        val numCols   = readInt()
-        val imageSize = numRows * numCols
 
-        val buf = new Array[Byte](imageSize)
-        (0 until numImages).iterator.map { _ =>
-          in.readFully(buf)
-          // Convert unsigned byte -> double in [0,1]
-          val doubles = buf.map(b => (b & 0xFF).toDouble / 255.0)
-          Vectors.dense(doubles)
+      // Parse header
+      val magic     = readInt()
+      require(magic == 2051, s"Expected magic 2051 for images, got $magic")
+      val numImages = readInt()
+      val numRows   = readInt()
+      val numCols   = readInt()
+      val imageSize = numRows * numCols
+
+      val buf = new Array[Byte](imageSize)
+      val out = new Array[Vector](numImages)
+      var i   = 0
+      while (i < numImages) {
+        in.readFully(buf)
+        // unsigned byte -> double in [0,1]
+        val doubles = new Array[Double](imageSize)
+        var j = 0
+        while (j < imageSize) {
+          doubles(j) = (buf(j) & 0xFF).toDouble / 255.0
+          j += 1
         }
-      } finally {
-        in.close()
+        out(i) = Vectors.dense(doubles)
+        i += 1
       }
+      in.close()
+      out.toIndexedSeq // eager, safe to return
     }.cache()
 
-    // Sanity check: make sure we have data
+    // Sanity check forces materialization
     val n = parsedData.count()
     require(n > 0, s"No images parsed from $path")
 
-    // --- K-Means ---
-    // MNIST: try k=10 (digits), 20–50 iterations is usually fine
+    // K-Means (good starting point for MNIST)
     val numClusters   = 10
     val numIterations = 40
-
-    // Optionally: KMeans|| init is default in recent Spark; older versions can set explicitly:
-    // val model = new KMeans().setK(numClusters).setMaxIterations(numIterations).setInitializationMode("k-means||").run(parsedData)
     val model = KMeans.train(parsedData, numClusters, numIterations)
 
     val WSSSE = model.computeCost(parsedData)
-    println(s"Images: $n  |  k=$numClusters  iters=$numIterations  |  WSSSE=$WSSSE")
+    println(s"Images: $n | k=$numClusters iters=$numIterations | WSSSE=$WSSSE")
 
     // Quick cluster size summary
     val clusterSizes = parsedData.map(v => model.predict(v)).countByValue().toSeq.sortBy(_._1)
