fc8dec41c3a069883fce1c113ba5a8593cd1d6f0
joaquin tomas <joaquintomas2003@gmail.com>
2025-09-17 21:13:55 -0300

    Filter out pure acks

--- packet-traces/capture_and_parse.zsh
+++ packet-traces/capture_and_parse.zsh
@@ -7,7 +7,7 @@ CSV_FILE="output.csv"
 DURATION="10s"   # capture duration
 
 echo "[*] Capturing packets on $IFACE for $DURATION..."
-sudo timeout "$DURATION" tcpdump_pfring -ni "$IFACE" -l | tee "$CAPTURE_FILE"
+sudo timeout "$DURATION" tcpdump_pfring -ni "$IFACE" -l 'tcp[13] & 0x17 != 0x10' | tee "$CAPTURE_FILE"
 
 echo "[*] Parsing packets into CSV..."
 python3 output_to_csv.py "$CAPTURE_FILE" "$CSV_FILE"
